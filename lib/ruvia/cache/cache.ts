90e3c1a17cfa5c43af74af37b04929ea4e69de00
zynoxslash <mehmetemintarlaci@gmail.com>
2024-12-11 18:00:02 +0300

    🌟 | Name updates and variable additions

--- lib/ruvia/cache/cache.ts
+++ lib/ruvia/cache/cache.ts
@@ -1,3 +1,5 @@
 import NC from "node-cache"
 
-export let cacheMessages = new NC({ stdTTL: 60, checkperiod: 30 })
+export const messageCache = new NC({ stdTTL: 60, checkperiod: 30 })
+
+export const buttonInteractionCache = new NC({ stdTTL: 10, checkperiod: 5 })
