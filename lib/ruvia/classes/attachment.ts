83a39493c4f61e9075462d76f12837c39ccd2c6a
zynoxslash <mehmetemintarlaci@gmail.com>
2024-12-22 21:39:30 +0300

    ✨ | New Method
    Added ‘fromBuffer’ method to get buffer data.

--- lib/ruvia/classes/attachment.ts
+++ lib/ruvia/classes/attachment.ts
@@ -6,11 +6,17 @@ import { Buffer } from "node:buffer";
 export class Attachment {
     public buffer: Buffer
 
-    constructor(filePath: string){
+    constructor(filePath?: string){
+        if(!filePath) return
         const src = path.join(Deno.cwd() as string, "./src", filePath);
         const buffer = readFileSync(src)
         this.buffer = buffer
         const attachment = new AttachmentBuilder(buffer, { name: filePath.split("/").pop() })
         return attachment
+    }
+
+    fromBuffer(fileName: string, buffer: Buffer){
+        this.buffer = buffer
+        return new AttachmentBuilder(buffer, { name: fileName })
     }
 }
